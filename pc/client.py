bf0185fa46ecf7d42c27f7deea0eb5ff9775f37f
Rizky I. F <rykzius@gmail.com>
2022-03-07 12:03:56 +0700

    Combine clients

--- pc/client.py
+++ pc/client.py
@@ -3,13 +3,22 @@ import os
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
+    client.subscribe("if4051-5-temperature")
+    client.subscribe("if4051-5-humidity-ground")
+    client.subscribe("if4051-5-humidity-air")
+    client.subscribe("if4051-5-light")
     client.subscribe("if4051-5-image")
 
 def on_message(client, userdata, msg):
-    print("Saved an image!")
-    f = open(next_path('image-%s.jpg'), 'wb')
-    f.write(msg.payload)
-    f.close()
+    sensor = msg.topic[9:]
+    if sensor == 'image':
+        path = next_path('./image-%s.jpg')
+        f = open(path, 'wb')
+        f.write(msg.payload)
+        f.close()
+        print(f"Received an image! Saved to {path}.")
+    else:
+        print(f"Received '{sensor}' sensor data: '{msg.payload.decode('utf-8')}'")
 
 def next_path(path_pattern):
     i = 1
@@ -17,7 +26,7 @@ def next_path(path_pattern):
         i = i * 2
     a, b = (i // 2, i)
     while a + 1 < b:
-        c = (a + b) // 2 # interval midpoint
+        c = (a + b) // 2
         a, b = (c, b) if os.path.exists(path_pattern % c) else (a, c)
     return path_pattern % b
 
